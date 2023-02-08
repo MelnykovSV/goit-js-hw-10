30c0052e2c11d5890e75fbbd3ee3ae4a53bc0248
MelnykovSV <svmelnykov@gmail.com>
2023-02-08 23:50:35 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,13 @@
 fetch('https://restcountries.com/v3.1/name/uk')
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
   .then(data => {
     console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
   });
