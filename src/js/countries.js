d25f86e5f4ef15764f312e566673801c1c9a7a56
MelnykovSV <svmelnykov@gmail.com>
2023-02-09 11:41:35 +0200

    Update countries.js

--- src/js/countries.js
+++ src/js/countries.js
@@ -36,13 +36,29 @@ function inputToQuery() {
 }
 
 function renderCard({ name, capital, population, flags, languages }) {
+  countryList.innerHTML = '';
   console.log(name.official);
   console.log(capital[0]);
   console.log(population);
   console.log(flags.svg);
   console.log(Object.values(languages));
+  const markup = `<div class="country-card__name-container"><img src="${
+    flags.svg
+  }" alt="Flag of ${
+    name.official
+  }" width="200" height="" /><p class="country-list__name">${
+    name.official
+  }</p></div><div class='country-card__capital'>${
+    capital[0]
+  }</div><div class='country-card__population'>${population}</div><div class='country-card__languages'>${Object.values(
+    languages
+  )}</div>`;
+
+  console.log(markup);
+  countryInfo.innerHTML = markup;
 }
 function renderList(arrayOfObjects) {
+  countryInfo.innerHTML = '';
   console.log('started');
   const listArray = arrayOfObjects.map(item => {
     const listData = {};
@@ -51,10 +67,9 @@ function renderList(arrayOfObjects) {
     return listData;
   });
   console.log(listArray);
-  //   let markup = '';
   const markup = listArray
     .map(item => {
-      return `<li class="country-list__item"><a href="#" class="country-list__link"><img src="${item.flag}" alt="Flag of ${item.name}" width="200" height="" /><p class="country-list__name">${item.name.official}</p></a></li>`;
+      return `<li class="country-list__item"><img src="${item.flag}" alt="Flag of ${name.official}" width="200" height="" /><p class="country-list__name">${item.name.official}</p></li>`;
     })
     .join('');
 
