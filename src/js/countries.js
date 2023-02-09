0421246118dfd376eada64ed0fc40078bb8423f6
MelnykovSV <svmelnykov@gmail.com>
2023-02-09 02:39:46 +0200

    list markup

--- src/js/countries.js
+++ src/js/countries.js
@@ -1,14 +1,65 @@
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+const debounce = require('lodash.debounce');
 const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-searchBox.addEventListener('input', () => {
-  const valueToSerach = inputToQuery();
-  fetchCountries(valueToSerach);
-});
+searchBox.addEventListener(
+  'input',
+  debounce(() => {
+    const valueToSearch = inputToQuery();
+
+    fetchCountries(valueToSearch)
+      .then(data => {
+        if (data.length === 1) {
+          console.log('found one country');
+          renderCard(data[0]);
+          return;
+        }
+        if (data.length > 10) {
+          console.log('to many countries found');
+          return;
+        }
+        // console.log(data);
+        renderList(data);
+        return data;
+      })
+      .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+  }, 300)
+);
 
 function inputToQuery() {
-  console.log(searchBox.value);
   return searchBox.value;
 }
 
-// fetchCountries('somerandomshit');
+function renderCard({ name, capital, population, flags, languages }) {
+  console.log(name.official);
+  console.log(capital[0]);
+  console.log(population);
+  console.log(flags.svg);
+  console.log(Object.values(languages));
+}
+function renderList(arrayOfObjects) {
+  console.log('started');
+  const listArray = arrayOfObjects.map(item => {
+    const listData = {};
+    listData.name = item.name;
+    listData.flag = item.flags.svg;
+    return listData;
+  });
+  console.log(listArray);
+  //   let markup = '';
+  const markup = listArray
+    .map(item => {
+      return `<li class="country-list__item"><a href="#" class="country-list__link"><img src="${item.flag}" alt="Flag of ${item.name}" width="200" height="" /><p class="country-list__name">${item.name.official}</p></a></li>`;
+    })
+    .join('');
+
+  countryList.innerHTML = markup;
+  console.log('markup: ' + markup);
+  console.log(done);
+  return listArray;
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,20 +1,10 @@
-import Notiflix from 'notiflix';
-
-export function fetchCountries(inputToSearch) {
-  fetch(
-    `https://restcountries.com/v3.1/name/${inputToSearch}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      return data;
-    })
-    .catch(error => {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+export function fetchCountries(valueToSearch) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${valueToSearch}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
